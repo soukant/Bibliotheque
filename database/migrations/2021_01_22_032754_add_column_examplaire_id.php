e4d55b0e3424d39ed04a5e7c2c7cb6b0920d7cd3
Anas Trak <anastarak9@gmail.com>
2021-01-22 04:31:35 +0100

    dd
    Co-Authored-By: soukant <63157691+soukant@users.noreply.github.com>

--- database/migrations/2021_01_22_032754_add_column_examplaire_id.php
+++ database/migrations/2021_01_22_032754_add_column_examplaire_id.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnUserId extends Migration
+class AddColumnExamplaireId extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,8 @@ class AddColumnUserId extends Migration
     public function up()
     {
         Schema::table('commandes', function (Blueprint $table) {
-            $table->BigInteger('user_id')->unsigned()->after('date');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->BigInteger('examplaire_id')->unsigned()->after('user_id');
+            $table->foreign('examplaire_id')->references('id')->on('examplaires');
         });
     }
 
@@ -27,8 +27,8 @@ class AddColumnUserId extends Migration
     public function down()
     {
         Schema::table('commandes', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
-            $table->dropColumn('user_id');
+            $table->dropForeign(['examplaire_id']);
+            $table->dropColumn('examplaire_id');
         });
     }
 }
